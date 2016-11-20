79fa87d202d38d119ba448cb81838cf60ff9cc38
nguyễn hoàng vĩnh thịnh <nthinh1102@gmail.com>
2016-11-20 16:06:48 +0700

    change code: diana is codename of 521

--- init/init_fame.cpp
+++ init/init_fame.cpp
@@ -89,8 +89,8 @@ void vendor_load_properties()
 							property_set("ro.product.model", "fame");
 						}
 						if (strcmp(modelnumber, "TYPE:RM-917\n") == 0) {
-							property_set("ro.product.device", "fame");
-							property_set("ro.product.model", "fame");
+							property_set("ro.product.device", "diana");
+							property_set("ro.product.model", "diana");
 						}
 						if (strcmp(modelnumber, "TYPE:RM-998\n") == 0) {
 							property_set("ro.product.device", "glee");
